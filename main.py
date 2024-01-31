11d20278b5028bb9470048ca0b4149cdef285e16
Binance GitHub <151542222+mybinancerepos@users.noreply.github.com>
2024-02-01 00:23:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import time
+import time, os
 from pymongo import MongoClient
 
 MONGO_URL = os.getenv("MONGO_URL")
