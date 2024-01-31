5d38f0c7a0d103c7a4f658a2609606e161e383dc
Binance GitHub <151542222+mybinancerepos@users.noreply.github.com>
2024-02-01 00:19:03 +0530

    check database

--- main.py
+++ main.py
@@ -4,6 +4,10 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import time
+from pymongo import MongoClient
+
+MONGO_URL = os.getenv("MONGO_URL")
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -11,8 +15,11 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    mongoClient = MongoClient(MONGO_URL)
+    db = mongoClient.TG_489567076
     while True:
-        print_hi('PyCharm')
+        collections = db.list_collection_names()
+        print ("collections:", collections, "\n")
         time.sleep(5)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
